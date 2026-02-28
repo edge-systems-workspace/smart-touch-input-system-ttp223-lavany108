2561869e746d4465a774c3ef5545079072f004cf
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-28 22:15:23 +0530

    added code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,24 @@
 #include <Arduino.h>
+
+const int touchPin = 7;
+const int ledPin = 13;
+
+void setup() {
+    pinMode(touchPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    int touchState = digitalRead(touchPin);
+
+    if (touchState == HIGH) {
+        digitalWrite(ledPin, HIGH);
+        Serial.println("Touched!!");
+    } else {
+        digitalWrite(ledPin, LOW);
+        Serial.println("Not touched");
+    }
+
+    delay(200);   // small delay to avoid flooding serial monitor
+}
